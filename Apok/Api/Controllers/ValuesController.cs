201d2a81e6e6c38506536b9399b109e0181a289b
oscar dev <oscar100493@gmail.com>
2024-12-10 14:39:46 -0400

    proyecto inicial base completado

---
+++ Apok/Api/Controllers/ValuesController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Api.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET: api/<ValuesController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+        
+    }
+}
